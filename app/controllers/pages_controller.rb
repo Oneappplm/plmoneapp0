fd9206eef69879c4ad381280b5b867597d8fc98a
iprog21 <iprog21@jaysonpresto.me>
2023-03-02 21:51:09 +0800

    Add more UI on VRC page work tickler and documents page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,9 @@
 class PagesController < ApplicationController
+	def virtual_review_committee
+		if params[:vrc].present? && params[:vrc] == 'work-tickler'
+			render 'work_tickler'
+		elsif params[:vrc].present? && params[:vrc] == 'documents'
+			render 'documents'
+		end
+	end
 end
