9bc4d9b34a0cfbe2f39745d0aca7e2354861edd9
iprog21 <iprog21@jaysonpresto.me>
2023-06-06 10:22:16 +0800

    enable role-based access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
-	# before_action :can_read?, only: [
-	# 	:index, :show, :overview, :dashboard, :client_portal, :provider_source, :plm_sales_tool,
-	# 	:organization_profile, :virtual_review_committee
-	# ]
-	# before_action :can_create?, only: [:new, :create]
-	# before_action :can_update?, only: [:edit, :update, :autosave]
-	# before_action :can_delete?, only: [:destroy]
+	before_action :can_read?, only: [
+		:index, :show, :overview, :dashboard, :client_portal, :provider_source, :plm_sales_tool,
+		:organization_profile, :virtual_review_committee
+	]
+	before_action :can_create?, only: [:new, :create]
+	before_action :can_update?, only: [:edit, :update, :autosave]
+	before_action :can_delete?, only: [:destroy]
 
 	before_action :authenticate_user!, except: %i[terms privacy_policy]
 	before_action :configure_permitted_parameters, if: :devise_controller?
