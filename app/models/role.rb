aad05c2aed15da757901e92cc175667de5eb9466
iprog21 <iprog21@jaysonpresto.me>
2023-06-09 08:08:40 +0800

    Add callbacks on roles creation and deletion sync to role-based access

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,6 +1,8 @@
 class Role < ApplicationRecord
  validates :name, uniqueness: true, presence: true
  before_validation :set_slug, on: :create
+	after_create :generate_role_based_access
+	after_destroy :delete_role_based_access
 
 	class << self
 		def load_data
@@ -14,4 +16,12 @@ class Role < ApplicationRecord
  def set_slug
   self.slug = name.parameterize(separator: '_')
  end
+
+	def generate_role_based_access
+		RoleBasedAccess.initialize_access(slug)
+	end
+
+	def delete_role_based_access
+		RoleBasedAccess.where(role: slug).destroy_all
+	end
 end
