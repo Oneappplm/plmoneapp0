99b9a3df65fdba6517cea03adae03dddefddad94
iprog21 <iprog21@jaysonpresto.me>
2023-06-21 10:41:54 +0800

    refactor sessions#create for recaptcha feature

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,28 +1,26 @@
 class Users::SessionsController < Devise::SessionsController
 	before_action :can_create?, except: [:new, :create]
 
-	def create
-  self.resource = warden.authenticate!(auth_options)
-  set_flash_message!(:notice, :signed_in)
-
-  if verify_recaptcha(model: resource) && resource.save
-    sign_in(resource_name, resource)
-    yield resource if block_given?
-    respond_with resource, location: after_sign_in_path_for(resource)
-  else
-    # Handle the case when reCAPTCHA verification fails
-    flash[:alert] = "reCAPTCHA verification failed."
-    # You may choose to render the sign-in form again or redirect somewhere else
-    redirect_to new_user_session_path
+  def create
+    if Rails.env.production?
+      if verify_recaptcha(model: resource)
+        self.resource = warden.authenticate!(auth_options)
+        set_flash_message!(:notice, :signed_in)
+        sign_in(resource_name, resource)
+        yield resource if block_given?
+        respond_with resource, location: after_sign_in_path_for(resource)
+        return
+      else
+        sign_out(resource_name)
+        # respond_with resource, location: after_sign_in_path_for(resource)
+        redirect_to new_user_session_path, alert: "reCAPTCHA verification failed." and return
+      end
+    else
+      self.resource = warden.authenticate!(auth_options)
+		  set_flash_message!(:notice, :signed_in)
+      sign_in(resource_name, resource)
+      yield resource if block_given?
+      respond_with resource, location: after_sign_in_path_for(resource)
+    end
   end
-end
-
-	def create_old
-		self.resource = warden.authenticate!(auth_options)
-		set_flash_message!(:notice, :signed_in)
-		# binding.break
-		sign_in(resource_name, resource)
-		yield resource if block_given?
-		respond_with resource, location: after_sign_in_path_for(resource)
-	end
 end
