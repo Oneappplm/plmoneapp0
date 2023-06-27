e44f4e73c2dfc2c876f6dceff1a2834a288f9836
Sidney John Diongzon <sidney@metromart.com>
2023-06-28 04:09:32 +0800

    Fill the data on view_summary page

--- app/controllers/view_summary_controller.rb
+++ app/controllers/view_summary_controller.rb
@@ -1,9 +1,13 @@
 class ViewSummaryController < ApplicationController
-
+	before_action :set_current_provider_source, only: [:index]
   def index
     if params[:page].present?
       render params[:page].to_s
     end
   end
 
+  private
+  def set_current_provider_source
+    @current_provider_source = ProviderSource.includes(:data).current
+  end
 end
