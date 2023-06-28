55a8d2452b11f9d66b85e7e104b0edfecb001577
iprog21 <iprog21@jaysonpresto.me>
2023-06-28 21:12:12 +0800

    refactor commands on git_deploy.rb

--- git_deploy.rb
+++ git_deploy.rb
@@ -1,9 +1,7 @@
 #!/usr/bin/env ruby
 
 puts `git checkout master && git pull origin master --rebase`
-puts `git fetch origin hvhs-app && git fetch origin qualifacts-app && git fetch origin staging`
-puts `git as -av`
-puts `git checkout hvhs-app && git push	origin hvhs-app &&	git push origin hvhs-app`
-puts `git checkout qualifacts-app && git push origin qualifacts-app && git push origin qualifacts-app`
-puts `git checkout staging && git push origin staging && git push origin staging`
-puts `git checkout master && git push origin master && git push origin master && git push heroku master`
+puts `git fetch origin hvhs-app && git checkout hvhs-app && git rebase master && git push	origin hvhs-app`
+puts `git fetch origin qualifacts-app && git checkout qualifacts-app && git rebase master && git push origin qualifacts-app`
+puts `git fetch origin staging && git checkout staging && git rebase master && git push origin staging`
+puts `git checkout master && git push origin master`
