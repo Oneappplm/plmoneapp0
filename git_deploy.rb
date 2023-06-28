fcbffde51676cfcd635dbcbfd4f51580818846aa
iprog21 <iprog21@jaysonpresto.me>
2023-06-28 21:02:19 +0800

    typo on git checkout origin master

--- git_deploy.rb
+++ git_deploy.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-puts `git checkout origin master && git pull origin master --rebase`
+puts `git checkout master && git pull origin master --rebase`
 puts `git fetch origin hvhs-app && git fetch origin qualifacts-app && git fetch origin staging`
 puts `git as -av`
 puts `git checkout hvhs-app && git push	origin hvhs-app &&	git push origin hvhs-app`
