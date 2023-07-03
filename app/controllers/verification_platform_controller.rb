6ac629eda8a91e7505ad7f07ef4db77ac534877d
iprog21 <iprog21@jaysonpresto.me>
2023-07-03 22:00:44 +0800

    HVHS data move to client home page #27

--- app/controllers/verification_platform_controller.rb
+++ app/controllers/verification_platform_controller.rb
@@ -1,7 +1,24 @@
 class VerificationPlatformController < ApplicationController
+  before_action :set_hvhs_datum, only: [:show, :edit, :update, :destroy]
+
   def index
     if params[:page]
       render params[:page]
+    else
+      @hvhs_data = if params[:search].present?
+        HvhsDatum.search(params[:search]).paginate(per_page: 10, page: params[:page] || 1)
+      else
+        HvhsDatum.paginate(per_page: 10, page: params[:page] || 1)
+      end
     end
+  end
+
+  def show
+    render 'overview'
+  end
+
+  protected
+  def set_hvhs_datum
+    @hvhs_datum = HvhsDatum.find(params[:id])
   end
 end
