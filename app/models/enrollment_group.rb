911dfdf025f21c97b753bdbe0cec24faee12177b
Sidney John Diongzon <sidney@metromart.com>
2023-07-15 21:42:15 +0800

    Remove group code on list details on enrollments/groups

--- app/models/enrollment_group.rb
+++ app/models/enrollment_group.rb
@@ -19,7 +19,7 @@ class EnrollmentGroup < ApplicationRecord
   validates_presence_of :group_name, :group_code, :office_address, :city, :state, :zip_code, :phone_number, :fax_number
 
   def dco_count_display
-    "#{dcos.size} #{group_code} clients"
+    "#{dcos.size} clients"
   end
 
   def selected_provider_types
