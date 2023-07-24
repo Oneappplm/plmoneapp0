37771da1181960f6472e99182ecf1df880458bf0
openroot258 <wisegeek00007@gmail.com>
2023-07-24 21:53:43 +0800

    Create new migration file to fix field error

--- db/migrate/20230716071642_add_more_field_to_enroll_groups_details.rb
+++ db/migrate/20230716071642_add_more_field_to_enroll_groups_details.rb
@@ -1,7 +1,5 @@
 class AddMoreFieldToEnrollGroupsDetails < ActiveRecord::Migration[7.0]
   def change
     add_column :enroll_groups_details, :payer_state, :string
-    add_column :enroll_groups_details, :payor_submission_type, :string
-    add_column :enroll_groups_details, :payor_link, :string
   end
 end
