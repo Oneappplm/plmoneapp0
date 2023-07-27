8373f7054a3c4d19480b01e17cb2765418b04689
iprog21 <iprog21@jaysonpresto.me>
2023-07-27 19:28:17 +0800

    Hotfix: bug on not showing location on practice information new provider page

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,8 +2,8 @@ class GroupsController < ApplicationController
   # get practice location
   def get_dcos
     enrollment_group = EnrollmentGroup.find_by(id: params[:group_id])
-    dcos ||= enrollment_group.dcos
-    arr_dcos ||= (dcos.blank? ? [] : dcos.pluck(:id, :dco_name))
+    dcos = enrollment_group.dcos
+    arr_dcos = (dcos.blank? ? [] : dcos.pluck(:id, :dco_name))
 
     render json: {
       "practice_locations" => arr_dcos
