d740bc2476eba8819e168ac170e3a81848f42e04
niuram.z3@gmail.com <niuram.z3@gmail.com>
2023-08-07 17:42:33 +0800

    Feature: load location based on selected group in searchform

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,7 +4,10 @@ class GroupsController < ApplicationController
     enrollment_group = EnrollmentGroup.find_by(id: params[:group_id])
     dcos = enrollment_group.dcos
     arr_dcos = (dcos.blank? ? [] : dcos.pluck(:id, :dco_name))
-
+    # this part will be used for search form in base_url/enrollment-clients since dco_name is being save in Provider table
+    if !params[:get_name].blank?
+      arr_dcos = (dcos.blank? ? [] : dcos.pluck(:dco_name, :dco_name))
+    end
     render json: {
       "practice_locations" => arr_dcos
     }
