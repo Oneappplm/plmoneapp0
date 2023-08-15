0a0605c8cb9576a773961a38291a4365a20769ae
niuram.z3@gmail.com <niuram.z3@gmail.com>
2023-08-15 19:13:40 +0800

    Hotfix: issue state and duplicate insurance carrier

--- app/models/enrollment_groups_detail.rb
+++ app/models/enrollment_groups_detail.rb
@@ -4,10 +4,4 @@ class EnrollmentGroupsDetail < ApplicationRecord
   def full_name
     "#{self.individual_ownership_first_name} #{self.individual_ownership_last_name}"
   end
-
-  def state
-    State.find_by(id: self.state_id)
-  rescue
-    nil
-  end
 end
