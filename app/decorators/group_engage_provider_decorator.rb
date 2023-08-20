508eb0d290de3ec035e818c658c21e8761886916
iprog21 <iprog21@jaysonpresto.me>
2023-08-20 21:32:05 +0800

    fix on adding provider without search result on group engage

--- app/decorators/group_engage_provider_decorator.rb
+++ app/decorators/group_engage_provider_decorator.rb
@@ -1,5 +1,5 @@
 class GroupEngageProviderDecorator < ApplicationDecorator
 	def date_of_birth
-		object.date_of_birth.strftime("%m/%d/%Y")
+		object.date_of_birth.strftime("%m/%d/%Y") rescue nil
 	end
 end
--- app/models/group_engage_provider.rb
+++ app/models/group_engage_provider.rb
@@ -9,6 +9,7 @@ class GroupEngageProvider < ApplicationRecord
 	belongs_to :practice_location, optional: true
 	has_one :provider_source
 
+	validates_presence_of :first_name, :last_name, :email_address, :ssn
 	validates_uniqueness_of :email_address, :ssn
 
 end
