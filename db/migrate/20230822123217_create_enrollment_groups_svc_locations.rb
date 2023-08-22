645184516cb200d362cd7a00a6ece97e01bca456
openroot258 <openroot258@gmail.com>
2023-08-22 23:17:38 +0800

    Move telehealth provider and admitting previlieges to Groups form | Remove Association start and end date

---
+++ db/migrate/20230822123217_create_enrollment_groups_svc_locations.rb
@@ -0,0 +1,19 @@
+class CreateEnrollmentGroupsSvcLocations < ActiveRecord::Migration[7.0]
+  def change
+    create_table :enrollment_groups_svc_locations do |t|
+      t.references :enrollment_groups
+      t.string :primary_service_non_office_area
+      t.string :primary_service_location_apps
+      t.string :primary_service_zip_code
+      t.string :primary_service_office_email
+      t.string :primary_service_fax
+      t.string :primary_service_office_website
+      t.string :primary_service_crisis_phone
+      t.string :primary_service_location_other_phone
+      t.string :primary_service_appt_scheduling
+      t.string :primary_service_interpreter_language
+      t.string :primary_service_telehealth_only_state
+      t.timestamps
+    end
+  end
+end
