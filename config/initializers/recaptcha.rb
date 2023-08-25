05b219b2078582740d54b952edd37fbb9e8b1c2b
iprog21 <iprog21@jaysonpresto.me>
2023-08-25 14:19:36 +0800

    Feature: Add Invisible Recaptcha

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,9 @@
 Recaptcha.configure do |config|
-	config.site_key  = '6Lc_ZbYmAAAAAEzp_jDGrnTh_M-oH61dLkn_JCil'
-	config.secret_key = '6Lc_ZbYmAAAAAMaGizrSGGeAwcLzrbDAjD5KsOfG'
+	# v2 visible
+	# config.site_key  = '6Lc_ZbYmAAAAAEzp_jDGrnTh_M-oH61dLkn_JCil'
+	# config.secret_key = '6Lc_ZbYmAAAAAMaGizrSGGeAwcLzrbDAjD5KsOfG'
+
+	# v2 invisible
+	config.site_key  = '6LcUudInAAAAAPceH-YwcNiRKBk0UcqqElaj81hX'
+	config.secret_key = '6LcUudInAAAAAGUi0LskW2AQYf5tlXW-KMCrFCCr'
 end
