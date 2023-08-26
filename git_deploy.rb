fc216fa826f802f454b8c7f93f80c1f806be5ba2
iprog21 <iprog21@jaysonpresto.me>
2023-08-26 17:37:12 +0800

    remove force push on other branches

--- git_deploy.rb
+++ git_deploy.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 puts `git checkout master && git pull origin master --rebase`
-puts `git fetch origin hvhs-app && git checkout hvhs-app && git rebase master && git push	origin hvhs-app -f`
-puts `git fetch origin qualifacts-app && git checkout qualifacts-app && git rebase master && git push origin qualifacts-app -f`
-puts `git fetch origin staging && git checkout staging && git rebase master && git push origin staging -f`
+puts `git fetch origin hvhs-app && git checkout hvhs-app && git rebase master && git push	origin hvhs-app`
+puts `git fetch origin qualifacts-app && git checkout qualifacts-app && git rebase master && git push origin qualifacts-app`
+puts `git fetch origin staging && git checkout staging && git rebase master && git push origin staging`
 puts `git checkout master && git push origin master`
