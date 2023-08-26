3f63837c441ab3b5a26d51710bf10f27e05d80d6
iprog21 <iprog21@jaysonpresto.me>
2023-08-26 17:36:13 +0800

    set devise session timeout to 2 hours

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -20,23 +20,5 @@ class Users::SessionsController < Devise::SessionsController
       yield resource if block_given?
       respond_with resource, location: after_sign_in_path_for(resource)
     end
-
-
-    # if Rails.env.production?
-    #   if verify_recaptcha(model: resource)
-    #     self.resource = warden.authenticate!(auth_options)
-    #     set_flash_message!(:notice, :signed_in)
-    #     sign_in(resource_name, resource)
-    #     yield resource if block_given?
-    #     respond_with resource, location: after_sign_in_path_for(resource)
-    #     return
-    #   else
-    #     sign_out(resource_name)
-    #     # respond_with resource, location: after_sign_in_path_for(resource)
-    #     redirect_to new_user_session_path, alert: "reCAPTCHA verification failed." and return
-    #   end
-    # else
-
-    # end
   end
 end
