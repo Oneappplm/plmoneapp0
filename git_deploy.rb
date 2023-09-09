74bb8ceb95d84dd45ca71fc234005637e2fdd6ab
iprog21 <iprog21@jaysonpresto.me>
2023-09-09 22:06:03 +0800

    update git_deploy.rb, comment staging branch and add qualifacts-app-heroku branch

--- git_deploy.rb
+++ git_deploy.rb
@@ -3,5 +3,6 @@
 puts `git checkout master && git pull origin master --rebase`
 puts `git fetch origin hvhs-app && git checkout hvhs-app && git rebase master && git push	origin hvhs-app`
 puts `git fetch origin qualifacts-app && git checkout qualifacts-app && git rebase master && git push origin qualifacts-app`
-puts `git fetch origin staging && git checkout staging && git rebase master && git push origin staging`
+puts `git fetch origin qualifacts-app-heroku && git checkout qualifacts-app-heroku && git rebase master && git push origin qualifacts-app-heroku`
+# puts `git fetch origin staging && git checkout staging && git rebase master && git push origin staging`
 puts `git checkout master && git push origin master`
