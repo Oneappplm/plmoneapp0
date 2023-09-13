d9b5e1b8c61759b69db1d0bcb046d42e337c5ed9
niuram.z3@gmail.com <niuram.z3@gmail.com>
2023-09-13 17:34:21 +0800

    Include svg in accepted image format and bring back staging to git_deploy

--- git_deploy.rb
+++ git_deploy.rb
@@ -4,5 +4,5 @@ puts `git checkout master && git pull origin master --rebase`
 puts `git fetch origin hvhs-app && git checkout hvhs-app && git rebase master && git push	origin hvhs-app -f`
 puts `git fetch origin qualifacts-app && git checkout qualifacts-app && git rebase master && git push origin qualifacts-app -f`
 puts `git fetch origin qualifacts-app-heroku && git checkout qualifacts-app-heroku && git rebase master && git push origin qualifacts-app-heroku -f`
-# puts `git fetch origin staging && git checkout staging && git rebase master && git push origin staging`
+puts `git fetch origin staging && git checkout staging && git rebase master && git push origin staging`
 puts `git checkout master && git push origin master -f`
