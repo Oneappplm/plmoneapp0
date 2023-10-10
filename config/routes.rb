830f18750f937c0c04c7ddc4ff772250c0b62a10
RajMankar-8 <rajmankar6858@gmail.com>
2023-10-10 19:14:42 +0530

    Practitioner/adding_form_and_practitioner

--- config/routes.rb
+++ config/routes.rb
@@ -80,6 +80,9 @@ Rails.application.routes.draw do
   post 'mark-notification-read', to: 'ajax#mark_notification_read'
   post 'logout-on-close', to: 'ajax#logout_on_close'
 
+  resources :manage_clients
+
+
   resources :provider_sources do
     collection do
       post :autosave
