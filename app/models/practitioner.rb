c4ebe0422b5a993b90f0a7dc0ae248b77bcbb1bb
RajMankar-8 <rajmankar6858@gmail.com>
2023-10-16 20:28:32 +0530

    adding upload and view button and pagination

--- app/models/practitioner.rb
+++ app/models/practitioner.rb
@@ -1,4 +1,5 @@
 class Practitioner < ApplicationRecord
+  self.per_page = 20
   validates :first_name, presence:true
   validates :last_name, presence: true
   validates :date_of_birth, presence: true
