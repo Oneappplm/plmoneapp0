aeb7bddb6263c0004de23473a56c5c9ad1d63fb0
iprog21 <iprog21@jaysonpresto.me>
2023-11-15 19:49:43 +0800

    rename boardnames.xlsx to board_names.xlsx

--- db/migrate/20230819155308_run_board_names_import_script.rb
+++ db/migrate/20230819155308_run_board_names_import_script.rb
@@ -1,5 +1,5 @@
 class RunBoardNamesImportScript < ActiveRecord::Migration[7.0]
   def change
-    Import::BoardNamesService.call(Rails.root.join('lib', 'data', 'boardnames.xlsx'))
+    Import::BoardNamesService.call(Rails.root.join('lib', 'data', 'board_names.xlsx'))
   end
 end
