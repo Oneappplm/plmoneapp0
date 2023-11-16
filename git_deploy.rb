328b1427f342e45d826a7009c74eb63224300ca4
iprog21 <iprog21@jaysonpresto.me>
2023-11-16 09:39:49 +0800

    add dcs-app and qualifacts-demo in git_deploy.rb for autodeployment

--- git_deploy.rb
+++ git_deploy.rb
@@ -3,5 +3,7 @@
 puts `git checkout master && git pull origin master --rebase`
 puts `git fetch origin hvhs-app && git checkout hvhs-app && git rebase master && git push	origin hvhs-app`
 puts `git fetch origin qualifacts-app && git checkout qualifacts-app && git rebase master && git push origin qualifacts-app`
+puts `git fetch origin qualifacts-demo && git checkout qualifacts-demo && git rebase qualifacts-app && git push origin qualifacts-demo`
 puts `git fetch origin qualifacts-app-heroku && git checkout qualifacts-app-heroku && git rebase master && git push origin qualifacts-app-heroku`
+puts `git fetch origin dcs-app && git checkout dcs-app && git rebase master && git push origin dcs-app`
 puts `git checkout master && git push origin master`
