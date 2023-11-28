ad05355559de500430a7d27c5b1391a4947060a7
iprog21 <iprog21@jaysonpresto.me>
2023-11-29 07:24:29 +0800

    Add method missing in user eigenclass

--- app/models/concerns/dynamic_roles_initializer.rb
+++ app/models/concerns/dynamic_roles_initializer.rb
@@ -3,8 +3,22 @@ module DynamicRolesInitializer
 
 	included do
 		def method_missing(method_name, *args, &block)
+			if method_name.to_s.end_with?("?")
 				role_name = method_name.to_s.chomp("?") # Remove trailing "?" if present
-				role.to_role == role_name.downcase
+				role.downcase.parameterize(separator: '_') == role_name.downcase
+			else
+				super
+			end
+		end
+
+		def self.method_missing(method_name, *args, &block)
+			if method_name.to_s.start_with?("pick_")
+				role_name = method_name.to_s.sub("pick_", "") # Remove leading "pick_" if present
+				role_name = role_name.downcase.parameterize(separator: '_')
+				find_by(user_role: role_name)
+			else
+				super
+			end
 		end
 	end
 end
