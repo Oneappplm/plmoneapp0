eb5bad11afee5546301e200fe038f5a16fa84fa6
iprog21 <iprog21@jaysonpresto.me>
2023-12-26 23:37:22 +0800

    add dcs-demo on auto deploy git script

--- git_deploy.rb
+++ git_deploy.rb
@@ -6,5 +6,6 @@ puts `git fetch origin qualifacts-app && git checkout qualifacts-app && git reba
 puts `git fetch origin qualifacts-demo && git checkout qualifacts-demo && git rebase qualifacts-app && git push origin qualifacts-demo`
 puts `git fetch origin qualifacts-app-heroku && git checkout qualifacts-app-heroku && git rebase master && git push origin qualifacts-app-heroku`
 puts `git fetch origin dcs-app && git checkout dcs-app && git rebase master && git push origin dcs-app`
+puts `git fetch origin dcs-demo && git checkout dcs-demo && git rebase dcs-app && git push origin dcs-demo`
 puts `git fetch origin metlife-app && git checkout metlife-app && git rebase master && git push origin metlife-app`
 puts `git checkout master && git push origin master`
