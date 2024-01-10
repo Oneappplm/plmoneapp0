104512610c4bc9b35287c34ea4a5af3932191b3d
RajMankar-8 <rajmankar6858@gmail.com>
2024-01-10 16:09:31 +0530

    approving record by directors

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,45 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_02_143959) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_08_222208) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
 
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "add_schedules", force: :cascade do |t|
+    t.string "group_name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "add_members", default: [], array: true
+  end
+  
   create_table "ahoy_events", force: :cascade do |t|
     t.bigint "visit_id"
     t.bigint "user_id"
@@ -155,6 +190,9 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_02_143959) do
     t.bigint "virtual_review_committee_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
+    t.string "description"
+    t.string "signature_upload"
     t.index ["user_id"], name: "index_director_providers_on_user_id"
     t.index ["virtual_review_committee_id"], name: "index_director_providers_on_virtual_review_committee_id"
   end
@@ -1810,6 +1848,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_02_143959) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "director_providers", "users"
   add_foreign_key "director_providers", "virtual_review_committees"
   add_foreign_key "egd_logs", "enroll_groups_details"
