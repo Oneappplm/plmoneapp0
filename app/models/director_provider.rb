08daa02f0d93fa6bf6a8964f6794e03be3036189
Ankit <ankitkusumakar55954@gmail.com>
2024-01-31 18:01:38 +0530

    fixed issue upload image

--- app/models/director_provider.rb
+++ app/models/director_provider.rb
@@ -1,4 +1,7 @@
 class DirectorProvider < ApplicationRecord
   belongs_to :user
   belongs_to :virtual_review_committee
+  enum status: ['Final Approval', 'Deny', 'Recommend termination', 'Pending', 'Tabled']
+  attribute :description, :string, default: 'Default Description'
+  attribute :signature_upload, :string, default: 'Default Signature Upload'
 end
