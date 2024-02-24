4499b7ca58ddf2e3d70524949f93d15ee7e5b442
iprog21 <iprog21@jaysonpresto.me>
2024-02-24 13:39:51 +0800

    Update git_deploy.rb comment all demo branch

--- git_deploy.rb
+++ git_deploy.rb
@@ -3,9 +3,9 @@
 puts `git checkout master && git pull origin master --rebase`
 puts `git fetch origin hvhs-app && git checkout hvhs-app && git rebase master && git push	origin hvhs-app`
 puts `git fetch origin qualifacts-app && git checkout qualifacts-app && git rebase master && git push origin qualifacts-app`
-puts `git fetch origin qualifacts-demo && git checkout qualifacts-demo && git rebase qualifacts-app && git push origin qualifacts-demo`
+# puts `git fetch origin qualifacts-demo && git checkout qualifacts-demo && git rebase qualifacts-app && git push origin qualifacts-demo`
 puts `git fetch origin qualifacts-app-heroku && git checkout qualifacts-app-heroku && git rebase master && git push origin qualifacts-app-heroku`
 puts `git fetch origin dcs-app && git checkout dcs-app && git rebase master && git push origin dcs-app`
-puts `git fetch origin dcs-demo && git checkout dcs-demo && git rebase dcs-app && git push origin dcs-demo`
+# puts `git fetch origin dcs-demo && git checkout dcs-demo && git rebase dcs-app && git push origin dcs-demo`
 puts `git fetch origin metlife-app && git checkout metlife-app && git rebase master && git push origin metlife-app`
 puts `git checkout master && git push origin master`
