c622a38307dd969c87f1b6ca033e892c8b30d1cb
iprog21 <iprog21@jaysonpresto.me>
2024-03-05 09:54:04 +0800

    Hotfix: Add more excluded atts in Audit Logs/Activity History

--- app/models/custom_audit.rb
+++ app/models/custom_audit.rb
@@ -1,17 +1,19 @@
 class CustomAudit < Audited::Audit
 	def dislay_changes
 			audited_changes.map do |attribute, changes|
-				next if changes[0].nil? && changes[1].nil? || changes[0] == changes[1] || ['encrypted_password', 'logout_on_close', 'last_logout_on_close'].include?(attribute)
+				next if changes.present? && changes[0].nil? && changes[1].nil? || changes[0] == changes[1] || excluded_atts.include?(attribute)
 
 				"<strong>#{attribute}</strong> changed from <strong style='color: red'>#{changes[0]}</strong> to <strong style='color: green'>#{changes[1]}</strong>"
 			end.compact
+	rescue
+		[]
 	end
 
 	def display_source
 		if auditable_type == 'Provider'
 			auditable.fullname
 		elsif auditable_type == 'EnrollmentProvidersDetail'
-			auditable.enrollment_provider.full_name
+			auditable&.enrollment_provider&.full_name
 		elsif auditable_type == 'User'
 			auditable.full_name
 		else
@@ -21,5 +23,17 @@ class CustomAudit < Audited::Audit
 
 	def empty_changes?
 		dislay_changes.empty?
+	end
+
+	def excluded_atts
+		[
+			'encrypted_password',
+			'logout_on_close',
+			'last_logout_on_close',
+			'agent_user_id',
+			'otp_token',
+			'otp_code',
+			'otp_code_expires_at'
+		]
 	end
 end
