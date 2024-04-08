2025de1628ea5b5323b6fa3b564df1ddc3d5bd80
iprog21 <iprog21@jaysonpresto.me>
2024-04-08 17:54:49 +0800

    MHM-1 | Remove Captcha

--- config/initializers/recaptcha.rb
+++
@@ -1,9 +0,0 @@
-Recaptcha.configure do |config|
-	# v2 visible
-	# config.site_key  = '6Lc_ZbYmAAAAAEzp_jDGrnTh_M-oH61dLkn_JCil'
-	# config.secret_key = '6Lc_ZbYmAAAAAMaGizrSGGeAwcLzrbDAjD5KsOfG'
-
-	# v2 invisible
-	config.site_key  = '6LcUudInAAAAAPceH-YwcNiRKBk0UcqqElaj81hX'
-	config.secret_key = '6LcUudInAAAAAGUi0LskW2AQYf5tlXW-KMCrFCCr'
-end
