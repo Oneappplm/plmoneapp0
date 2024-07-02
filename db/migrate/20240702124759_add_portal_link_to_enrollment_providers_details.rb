0d83a5422afbbc54f10491bd162ff923e7ece994
pawansikarwar0207 <pawansikarwar96@gmail.com>
2024-07-02 19:06:13 +0530

    qual-57/added-missing-field

--- db/migrate/20240702124759_add_portal_link_to_enrollment_providers_details.rb
+++ db/migrate/20240702124759_add_portal_link_to_enrollment_providers_details.rb
@@ -1,5 +1,5 @@
 class AddPortalLinkToEnrollmentProvidersDetails < ActiveRecord::Migration[7.0]
   def change
-    add_column :enrollment_providers_details, :portal_link, :string
+    add_column :enrollment_providers_details, :portal_link, :string, if_not_exists: true
   end
 end
