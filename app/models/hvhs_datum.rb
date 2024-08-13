84b6e3f282049dc09a1c00b17024cefa2b9543b7
Ankit <ankitkusumakar55954@gmail.com>
2024-08-13 14:02:16 +0530

    In this “Manage DB” we need to add the search functionality

--- app/models/hvhs_datum.rb
+++ app/models/hvhs_datum.rb
@@ -1,10 +1,15 @@
 class HvhsDatum < ApplicationRecord
   include PgSearch::Model
-	pg_search_scope :search,
-          against: self.column_names,
-          using: {
-            tsearch: {any_word: true}
-          } rescue nil
 
-  def fullname = "#{first_name} #{middle_name} #{last_name}"
+  # Define a pg_search_scope that searches across specific columns
+  pg_search_scope :search,
+    against: [:first_name, :middle_name, :last_name],
+    using: {
+      tsearch: { prefix: true, any_word: true }
+    }
+
+  # Method to return the fullname
+  def fullname
+    "#{first_name} #{middle_name} #{last_name}".strip
+  end
 end
