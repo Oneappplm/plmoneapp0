0c317bd9a5679098e8f2c6b1fa6b1a089dbed2fa
Ankit <ankitkusumakar55954@gmail.com>
2024-08-13 14:29:46 +0530

    i added the search functionality. Manage DB and client home

--- app/models/hvhs_datum.rb
+++ app/models/hvhs_datum.rb
@@ -1,10 +1,25 @@
 class HvhsDatum < ApplicationRecord
   include PgSearch::Model
-	pg_search_scope :search,
-          against: self.column_names,
-          using: {
-            tsearch: {any_word: true}
-          } rescue nil
 
-  def fullname = "#{first_name} #{middle_name} #{last_name}"
+  # Define a pg_search_scope that searches across the full concatenated name
+  pg_search_scope :search,
+    against: [:first_name, :middle_name, :last_name],
+    using: {
+      tsearch: {
+        prefix: true,
+        any_word: true,
+        dictionary: "english"
+      }
+    },
+    ignoring: :accents
+
+  # Override the scope to search across concatenated fields
+  def self.search(query)
+    where("CONCAT_WS(' ', first_name, middle_name, last_name) ILIKE ?", "%#{query}%")
+  end
+
+  # Method to return the fullname
+  def fullname
+    "#{first_name} #{middle_name} #{last_name}".strip
+  end
 end
