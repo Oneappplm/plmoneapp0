ea57f7837823a18945d0aee58f276e78e280922d
Sidney John Diongzon <pikot08@gmail.com>
2024-09-20 01:29:15 +0800

    add updating of practitioner info, adding of education

---
+++ app/controllers/mhc/provider_personal_informations_controller.rb
@@ -0,0 +1,41 @@
+class Mhc::ProviderPersonalInformationsController < ApplicationController
+  before_action :set_provider_personal_information, only: [:update]
+
+  def update
+    @provider_personal_information.assign_attributes(provider_personal_information_params)
+
+    if @provider_personal_information.save
+      redirect_to mhc_verification_platform_path(page_tab: 'practitioner_info',id: params[:provider_personal_information][:provider_attest_id]), notice: 'Practice information saved successfully.'
+    else
+      redirect_to mhc_verification_platform_path(page_tab: 'practitioner_info',id: params[:provider_personal_information][:provider_attest_id]), alert: 'Failed to save practice information.'
+    end
+  end
+
+  private
+
+  def set_provider_personal_information
+    @provider_personal_information = ProviderPersonalInformation.find_by(provider_attest_id: params[:provider_personal_information][:provider_attest_id])
+  end
+
+  # Strong parameters for security
+  def provider_personal_information_params
+    params.require(:provider_personal_information).permit(
+      :id, :caqh_provider_id, :provider_attest_id, :caqh_provider_attest_id, :last_name, :first_name,
+      :middle_name, :suffix, :primary_practice_state, :other_name_flag, :birth_date, :us_eligible_flag,
+      :ssn, :nid, :dea_flag, :cds_flag, :upin, :upin_flag, :npi_flag, :npi, :medicare_provider_flag,
+      :medicaid_provider_flag, :other_graduate_education_flag, :fellowship_training_flag, :teaching_appointment_flag,
+      :secondary_specialty_flag, :other_specialty_flag, :hospital_privilege_flag, :hospital_admitting_arrangements,
+      :work_history_gap_flag, :active_military_flag, :citizenship_status, :visa_number, :federal_employee_id,
+      :no_malpractice_claims_flag, :application_type, :ecfmg_flag, :ecfmg_number, :ecfmg_issue_date,
+      :hospital_based_flag, :email_address, :visa_type, :visa_status, :birth_city, :birth_state,
+      :tax_id, :spouse_last_name, :spouse_first_name, :other_correspondence_address,
+      :emergency_contact_last_name, :emergency_contact_first_name, :emergency_contact_middle_name,
+      :emergency_contact_phone, :pager_beeper_number, :answering_service_phone_number, :cell_phone_number,
+      :pager_beeper_digital_flag, :visa_expiration_date, :ethnicity_description, :visa_issue_date,
+      :ecfmg_expiration_date, :work_permit_status, :spouse_middle_name,
+      :state_residence_date, :citizenship_country_country_name, :marital_status_marital_status_description,
+      :gender_gender_description, :birth_country_country_name, :correspondence_address_type_correspondence_address_type_descrip,
+      :provider_type_provider_type_abbreviation, :graduate_type_graduate_type_description,
+      :nid_country_country_name, :attest_date, :plan_provider_id, :last_recredential_date, :next_recredential_date)
+  end
+end
--- app/controllers/mhc/verification_platform_controller.rb
+++ app/controllers/mhc/verification_platform_controller.rb
@@ -8,9 +8,7 @@ class Mhc::VerificationPlatformController < ApplicationController
 
   def show
     if params[:page_tab]
-      if params[:page_tab] == 'add_practice_info'
-        @practice_information = PracticeInformation.new(provider_attest_id: @provider_personal_information.provider_attest_id, caqh_provider_attest_id: @provider_personal_information.caqh_provider_attest_id )
-      end
+      get_data
 
       render params[:page_tab]
     else
@@ -20,13 +18,30 @@ class Mhc::VerificationPlatformController < ApplicationController
 
   protected
   def set_provider_personal_informations
-    @provider_personal_information = ProviderPersonalInformation.find(params[:id])
-    @practice_informations = @provider_personal_information.provider_attest.practice_informations
+    @provider_personal_information = ProviderPersonalInformation.find_by(provider_attest_id: params[:id])
   end
 
-		def redirect_to_auto_verify
-			if current_setting.dcs?
-				redirect_to auto_verifies_path and return
-			end
-		end
+  def get_data
+    if params[:page_tab] == 'add_practice_info'
+      @practice_information = PracticeInformation.new(provider_attest_id: @provider_personal_information.provider_attest_id, caqh_provider_attest_id: @provider_personal_information.caqh_provider_attest_id )
+    end
+
+    if params[:page_tab] == 'add_education_info'
+      @provider_education = ProviderEducation.new(provider_attest_id: @provider_personal_information.provider_attest_id, caqh_provider_attest_id: @provider_personal_information.caqh_provider_attest_id )
+    end
+
+    if params[:page_tab] == 'practice_info'
+      @practice_informations = @provider_personal_information.provider_attest.practice_informations.paginate(per_page: 10, page: params[:page])
+    end
+
+    if params[:page_tab] == 'education'
+      @provider_educations = @provider_personal_information.provider_attest.provider_educations.paginate(per_page: 10, page: params[:page])
+    end
+  end
+
+  def redirect_to_auto_verify
+    if current_setting.dcs?
+      redirect_to auto_verifies_path and return
+    end
+  end
 end
