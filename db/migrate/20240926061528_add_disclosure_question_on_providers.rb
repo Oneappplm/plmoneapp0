e12e34cec8b8313871c21215984545be1b15b8da
Ankit <ankitkusumakar55954@gmail.com>
2024-09-26 12:28:01 +0530

    Added disclosure question

---
+++ db/migrate/20240926061528_add_disclosure_question_on_providers.rb
@@ -0,0 +1,30 @@
+class AddDisclosureQuestionOnProviders < ActiveRecord::Migration[7.0]
+  def change
+    add_column :providers, :disclosure_question1, :string
+    add_column :providers, :disclosure_question2, :string
+    add_column :providers, :disclosure_question3, :string
+    add_column :providers, :disclosure_question4, :string
+    add_column :providers, :disclosure_question5, :string
+    add_column :providers, :disclosure_question6, :string
+    add_column :providers, :disclosure_question7, :string
+    add_column :providers, :disclosure_question8, :string
+    add_column :providers, :disclosure_question9, :string
+    add_column :providers, :disclosure_question10, :string
+    add_column :providers, :disclosure_question11, :string
+    add_column :providers, :disclosure_question12, :string
+    add_column :providers, :disclosure_question13, :string
+    add_column :providers, :disclosure_question14, :string
+    add_column :providers, :disclosure_question15, :string
+    add_column :providers, :disclosure_question16, :string
+    add_column :providers, :disclosure_question17, :string
+    add_column :providers, :disclosure_question18, :string
+    add_column :providers, :disclosure_question19, :string
+    add_column :providers, :disclosure_question20, :string
+    add_column :providers, :disclosure_question21, :string
+    add_column :providers, :disclosure_question22, :string
+    add_column :providers, :disclosure_question23, :string
+    add_column :providers, :disclosure_question24, :string
+    add_column :providers, :disclosure_question25, :string
+    add_column :providers, :disclosure_question26, :string
+  end
+end
