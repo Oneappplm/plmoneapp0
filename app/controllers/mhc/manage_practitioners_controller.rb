e03fff84d2821c8956d2937a1c90e1de3c6f911f
Sidney John Diongzon <pikot08@gmail.com>
2024-12-24 00:27:23 +0800

    Fix issue on manage practitioner page

--- app/controllers/mhc/manage_practitioners_controller.rb
+++ app/controllers/mhc/manage_practitioners_controller.rb
@@ -1,5 +1,6 @@
 class Mhc::ManagePractitionersController < ApplicationController
   def index
-    @provider_personal_informations = ProviderPersonalInformation.paginate(per_page: 10, page: params[:page] || 1)
+    @q = ProviderPersonalInformation.ransack(params[:q])
+    @provider_personal_informations = @q.result(distinct: true).paginate(per_page: 10, page: params[:page] || 1)
   end
 end
