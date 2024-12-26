9f03e5d448235434b30120059b647a9f57a5ab08
Sidney John Diongzon <pikot08@gmail.com>
2024-12-26 17:27:19 +0800

    Add app tracking tab

--- app/models/provider_personal_information.rb
+++ app/models/provider_personal_information.rb
@@ -25,6 +25,8 @@ class ProviderPersonalInformation < ApplicationRecord
   has_many :provider_personal_information_sam_records
   has_many :provider_personal_information_reinstatements
   has_many :provider_personal_information_comments
+  has_many :provider_personal_information_app_trackings
+
   has_one :provider_personal_information_credentialing_contact
   has_one :provider_personal_information_confidential_contact
 
