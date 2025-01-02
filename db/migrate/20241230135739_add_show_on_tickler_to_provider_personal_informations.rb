7320f5eb6f241f9056495526ed93c62f539fe316
pawansikarwar0207 <pawansikarwar96@gmail.com>
2025-01-02 16:57:56 +0530

    quick fix for migration

--- db/migrate/20241230135739_add_show_on_tickler_to_provider_personal_informations.rb
+++
@@ -1,5 +0,0 @@
-class AddShowOnTicklerToProviderPersonalInformations < ActiveRecord::Migration[7.0]
-  def change
-    add_column :provider_personal_informations, :show_on_tickler, :boolean
-  end
-end
