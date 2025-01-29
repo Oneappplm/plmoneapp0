646597cb521ba25352f2ea5b54ee8bef41ad8482
IPROG TECH <iprog.tech@gmail.com>
2025-01-29 19:45:06 +0800

    Feature: Help Codes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_01_24_113343) do
+ActiveRecord::Schema[7.0].define(version: 2025_01_28_132806) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -727,6 +727,14 @@ ActiveRecord::Schema[7.0].define(version: 2025_01_24_113343) do
 
   create_table "health_plans", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "help_codes", force: :cascade do |t|
+    t.string "category"
+    t.string "code"
+    t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
