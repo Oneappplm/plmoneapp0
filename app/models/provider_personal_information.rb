d3ba23d96b163fb8797892b8dba0c654403286bc
pawansikarwar0207 <pawansikarwar96@gmail.com>
2025-02-05 15:10:36 +0530

    Quick fixes

--- app/models/provider_personal_information.rb
+++ app/models/provider_personal_information.rb
@@ -1,9 +1,18 @@
 class ProviderPersonalInformation < ApplicationRecord
-  self.primary_key = :provider_id
+
+  PRIMARY_KEY_ROW_NAMES = ['ProviderAttestID','ProviderID']
 
   belongs_to :provider_attest
 
-  validates :provider_attest, presence: true
+  validates :provider_attest_id, presence: true
 
   def fullname = "#{last_name}, #{first_name} #{middle_name}, #{provider_type_provider_type_abbreviation}"
+
+  before_validation :set_provider_attest
+  private
+
+  def set_provider_attest
+    self.provider_attest = ProviderAttest.where(caqh_provider_attest_id: self.caqh_provider_attest_id).last
+  end
 end
+
