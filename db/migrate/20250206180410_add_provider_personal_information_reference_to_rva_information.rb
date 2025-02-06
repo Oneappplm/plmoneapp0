1340739831c472747fbddee41e1f46706bc512b0
pawansikarwar0207 <pawansikarwar96@gmail.com>
2025-02-07 01:38:28 +0530

    Quick fix for migration

--- db/migrate/20250206180410_add_provider_personal_information_reference_to_rva_information.rb
+++ db/migrate/20250206180410_add_provider_personal_information_reference_to_rva_information.rb
@@ -1,5 +1,5 @@
 class AddProviderPersonalInformationReferenceToRvaInformation < ActiveRecord::Migration[7.0]
   def change
-    add_reference :rva_informations, :provider_personal_information, null: false, foreign_key: true
+    add_reference :rva_informations, :provider_personal_information, null: true, foreign_key: true
   end
 end
