2fb5d7464695de049d4c93d51065caad09959b0e
pawansikarwar0207 <pawansikarwar96@gmail.com>
2025-02-07 20:12:16 +0530

    added master & review list in app tracking of dcm

---
+++ db/migrate/20250207144030_add_master_lists_to_provider_personal_information_app_tracking.rb
@@ -0,0 +1,6 @@
+class AddMasterListsToProviderPersonalInformationAppTracking < ActiveRecord::Migration[7.0]
+  def change
+    add_column :provider_personal_information_app_trackings, :master_issues, :string, array: true, default: []
+    add_column :provider_personal_information_app_trackings, :master_reviews, :string, array: true, default: []
+  end
+end
