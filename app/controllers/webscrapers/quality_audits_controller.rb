d80426cf0fdec58474b59f6cb98eb7b4fc1ab2da
Ankit <ankitkusumakar55954@gmail.com>
2025-02-13 10:11:03 +0530

    fixed EDUCATION and BOARD Qulity audit

--- app/controllers/webscrapers/quality_audits_controller.rb
+++ app/controllers/webscrapers/quality_audits_controller.rb
@@ -184,4 +184,38 @@ class Webscrapers::QualityAuditsController < ApplicationController
   rescue => e
     render json: { error: e.message }, status: :unprocessable_entity
   end
+
+  def send_education_request
+    last_name = params[:last_name] 
+    first_name = params[:first_name]
+    # Create reva informaton for send request 
+    rva_information = RvaInformation.create(tab: 'EDUCATION', send_request: 'SENT', requested_by: first_name, requested_date: Date.today, requested_method: 'Letter', required_fee_amount: 0, check_generated: false, received_by: first_name, received_status: true, comments: 'none', received_date: Date.today)
+    render json: { message: 'requestsent sccessfully',  rva_information: rva_information}, status: :ok
+  rescue => e
+    render json: { error: e.message }, status: :unprocessable_entity
+  end
+  
+  def send_board_cert_request
+    last_name = params[:last_name] 
+    first_name = params[:first_name]
+
+    # Create RVA information for NPDB request
+    rva_information = RvaInformation.create(
+      tab: 'BOARD CERT',
+      send_request: 'SENT',
+      requested_by: first_name,
+      requested_date: Date.today,
+      requested_method: 'Letter',
+      required_fee_amount: 0,
+      check_generated: false,
+      received_by: first_name,
+      received_status: true,
+      comments: 'none',
+      received_date: Date.today
+    )
+
+    render json: { message: 'board cert request sent successfully', rva_information: rva_information }, status: :ok
+  rescue => e
+    render json: { error: e.message }, status: :unprocessable_entity
+  end  
 end
