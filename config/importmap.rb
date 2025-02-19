2aeafaac3c3f275fffb1f6be6f906e10e9129ed5
pawansikarwar0207 <pawansikarwar96@gmail.com>
2025-02-19 15:38:49 +0530

    added the faq section in dcm

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,3 +4,4 @@ pin "application", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin "ahoy", to: "ahoy.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "@rails/ujs", to: "@rails--ujs.js" # @7.1.3
