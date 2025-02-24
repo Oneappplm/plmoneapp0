ef986a51b61839247adc503d0bd6188afa937528
pawansikarwar0207 <pawansikarwar96@gmail.com>
2025-02-24 18:18:43 +0530

    fixed issue in migration for app tracker in dcm

--- db/migrate/20240909172145_create_provider_personal_docs_uploads.rb
+++ db/migrate/20240909172145_create_provider_personal_docs_uploads.rb
@@ -2,10 +2,14 @@ class CreateProviderPersonalDocsUploads < ActiveRecord::Migration[7.0]
   def change
     create_table :provider_personal_docs_uploads do |t|
       t.string :file_upload
-      t.references :provider_attest, null: false, foreign_key: { to_table: :provider_attests, primary_key: :provider_attest_id }
-      t.integer :provider_id
+      t.references :provider_attest
+      t.integer    :caqh_provider_attest_id
+      t.references :provider_personal_information, index: false
+      t.integer :caqh_provider_id
 
       t.timestamps
     end
+    add_index  :provider_personal_docs_uploads, :provider_personal_information_id, :name => 'ppdu_ppi_id'
+
   end
 end
