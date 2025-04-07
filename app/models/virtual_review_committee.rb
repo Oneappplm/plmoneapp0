2d3af474095cf4d9461f6246f417558238dddbf6
pawansikarwar0207 <pawansikarwar96@gmail.com>
2025-04-07 22:20:33 +0530

    vrc on dcm phase1

--- app/models/virtual_review_committee.rb
+++ app/models/virtual_review_committee.rb
@@ -18,4 +18,8 @@ class VirtualReviewCommittee < ApplicationRecord
 	def social
 		"@#{self.provider_name.parameterize}"
 	end
+
+	def vrc_full_name
+		"#{self.provider_name&.split(',')&.first || ''} #{self.provider_name&.split(',')&.last || ''}"
+	end
 end
---
+++ app/models/vrc_document.rb
@@ -0,0 +1,3 @@
+class VrcDocument < ApplicationRecord
+	mount_uploader :file_upload, DocumentUploader
+end
