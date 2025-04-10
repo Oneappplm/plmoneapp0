f420d07bea260ad8d870b438e54ae21dfc6c19ff
pawansikarwar0207 <pawansikarwar96@gmail.com>
2025-04-11 00:11:06 +0530

    client portal on dcm

--- db/migrate/20250405143318_create_vrc_documents.rb
+++ db/migrate/20250405143318_create_vrc_documents.rb
@@ -1,11 +1,13 @@
 class CreateVrcDocuments < ActiveRecord::Migration[7.0]
   def change
-    create_table :vrc_documents do |t|
-      t.string :name
-      t.date :committee_date
-      t.string :file_upload
+    unless table_exists?(:vrc_documents)
+      create_table :vrc_documents do |t|
+        t.string :name
+        t.date :committee_date
+        t.string :file_upload
 
-      t.timestamps
+        t.timestamps
+      end
     end
   end
 end
