18257b493ec957fdfd7bee2dcc4c0e6b642fa998
Ankit <ankitkusumakar55954@gmail.com>
2025-04-18 17:52:30 +0530

    fixed Liability skip rva

--- config/routes.rb
+++ config/routes.rb
@@ -426,6 +426,7 @@ Rails.application.routes.draw do
     post '/send_employment_skip_rva', to: 'quality_audits#send_employment_skip_rva'
     post '/send_npdb_skip_rva', to: 'quality_audits#send_npdb_skip_rva'
     post '/send_board_cert_skip_rva', to: 'quality_audits#send_board_cert_skip_rva'
+    post '/send_liability_skip_rva', to: 'quality_audits#send_liability_skip_rva'
     resources :alaska_states, only: [:index], path: 'state-alaska' do
       collection do
         get :crawl
