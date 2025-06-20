8e69ddaf881fc571e4592d03619868990b497d0e
pawansikarwar0207 <pawansikarwar96@gmail.com>
2025-06-21 00:43:20 +0530

    fixed issue, update ruby & rails version on dcm-app

--- db/migrate/20250128132806_create_help_codes.rb
+++ db/migrate/20250128132806_create_help_codes.rb
@@ -1,6 +1,6 @@
 class CreateHelpCodes < ActiveRecord::Migration[7.0]
   def change
-    create_table :help_codes do |t|
+    create_table :help_codes, if_not_exists: true do |t|
       t.string :category
       t.string :code
       t.string :description
