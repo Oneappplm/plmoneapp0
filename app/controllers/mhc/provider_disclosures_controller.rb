3d1fe5976a5d8b9fcb00f587c92844b4a0eb3979
pawansikarwar0207 <pawansikarwar96@gmail.com>
2025-09-11 23:20:14 +0530

    Fixed disclosure tab issue

--- app/controllers/mhc/provider_disclosures_controller.rb
+++ app/controllers/mhc/provider_disclosures_controller.rb
@@ -1,29 +1,31 @@
 class Mhc::ProviderDisclosuresController < ApplicationController
-  def new
-    @provider_personal_information = ProviderPersonalInformation.find(params[:id])
-    @provider_attest = @provider_personal_information.provider_attest
-    8.times { @provider_attest.provider_disclosures.build } if @provider_attest.provider_disclosures.empty?
-  end
-  
   def create
     @provider_personal_information = ProviderPersonalInformation.find(params[:id])
     provider_attest = @provider_personal_information.provider_attest
 
     disclosures_params = provider_attest_params[:provider_disclosures_attributes]
 
-    disclosures_params&.each do |_idx, disclosure_attrs|
-      if disclosure_attrs[:id].present?
-        # Try to find existing record
-        disclosure = provider_attest.provider_disclosures.where(id: disclosure_attrs[:id]).first
-        if disclosure
-          disclosure.update(disclosure_attrs.except(:id))
-        else
-          # If ID doesn’t belong, create new
-          provider_attest.provider_disclosures.create(disclosure_attrs.except(:id))
-        end
+    disclosures_params&.each do |_idx, attrs|
+      # --- convert "true"/"false" strings to real booleans or nil ---
+      if attrs.key?(:disclosure_answer_flag)
+        attrs[:disclosure_answer_flag] =
+          case attrs[:disclosure_answer_flag]
+          when true, 'true', '1'   then true
+          when false, 'false', '0' then false
+          else nil
+          end
+      end
+
+      if attrs[:id].present?
+        disclosure = provider_attest.provider_disclosures.find_or_initialize_by(id: attrs[:id])
+        # keep created date in disclosure_date when updating
+        attrs[:disclosure_date] = disclosure.created_at if disclosure.persisted?
+        disclosure.update(attrs.except(:id))
       else
-        # New record
-        provider_attest.provider_disclosures.create(disclosure_attrs)
+        # new record: set disclosure_date to created_at after save
+        d = provider_attest.provider_disclosures.build(attrs.except(:id))
+        d.save
+        d.update_column(:disclosure_date, d.created_at)
       end
     end
 
@@ -36,8 +38,12 @@ class Mhc::ProviderDisclosuresController < ApplicationController
   def provider_attest_params
     params.require(:provider_attest).permit(
       provider_disclosures_attributes: [
-        :id, :disclosure_question_disclosure_summary, :disclosure_answer_flag, 
-        :disclosure_explanation, :disclosure_date, :_destroy
+        :id,
+        :disclosure_question_disclosure_summary,
+        :disclosure_answer_flag,
+        :disclosure_explanation,
+        :disclosure_date,
+        :_destroy
       ]
     )
   end
