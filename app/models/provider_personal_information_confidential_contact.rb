79545065f3a5266121eb3eea13c211b80cb128ee
pawansikarwar0207 <pawansikarwar96@gmail.com>
2025-09-16 22:58:14 +0530

    hvhs provider verification and audit issues V2 phase2

--- app/models/provider_personal_information_confidential_contact.rb
+++ app/models/provider_personal_information_confidential_contact.rb
@@ -2,6 +2,6 @@ class ProviderPersonalInformationConfidentialContact < ApplicationRecord
   belongs_to :provider_personal_information
 
   def complete_address
-    "#{address} #{address2} #{city} #{country} #{state} #{zip}"
+    "#{address} #{address2} #{city} #{country}"
   end
 end
