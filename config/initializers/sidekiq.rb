717fac1118b04b61e9862375a88261dc268890d4
pawansikarwar0207 <pawansikarwar96@gmail.com>
2025-10-15 17:57:37 +0530

    Fixed sidekiq slow issue

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,4 +1,4 @@
-redis_url = ENV.fetch("REDIS_URL") { "redis://default:65a314d18ddc074608e003c3@10.0.1.213:6379/0" }
+redis_url = ENV.fetch("REDIS_URL") { "redis://127.0.0.1:6379/0" }
 
 Sidekiq.configure_server do |config|
   config.redis = { url: redis_url }
