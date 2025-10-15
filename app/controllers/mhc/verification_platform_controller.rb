64e07046bad4048027bb0eaa9080fb8475bbd68e
pawansikarwar0207 <pawansikarwar96@gmail.com>
2025-10-15 17:57:37 +0530

    Fixed sidekiq slow issue

--- app/controllers/mhc/verification_platform_controller.rb
+++ app/controllers/mhc/verification_platform_controller.rb
@@ -3,10 +3,9 @@ class Mhc::VerificationPlatformController < ApplicationController
   before_action :redirect_to_auto_verify, only: [:index]
 
   def index
-    @q = ProviderPersonalInformation
-       .where(ProviderPersonalInformation.arel_table[:cred_status].eq(nil)
-              .or(ProviderPersonalInformation.arel_table[:cred_status].not_eq('no-application')))
-       .ransack(params[:q])
+    @q = ProviderPersonalInformation.where.not(cred_status: 'no-application').or(
+       ProviderPersonalInformation.where(cred_status: nil)
+     ).ransack(params[:q])
     @provider_personal_informations = @q.result(distinct: true).paginate(per_page: 10, page: params[:page] || 1)
     @client_organizations = ClientOrganization.all
   end
