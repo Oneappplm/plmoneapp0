8bea6f4dbfb9c4385b65b888cf87c7e39186f061
pawansikarwar0207 <pawansikarwar96@gmail.com>
2025-10-21 18:26:51 +0530

    fixed changes in service file for hippocrates phase3

--- app/services/chat_gpt_service.rb
+++ app/services/chat_gpt_service.rb
@@ -4,7 +4,7 @@ require "openai"
 class ChatGptService
   def initialize
     @client = OpenAI::Client.new(
-      access_token: Rails.application.credentials.dig(:openai, :api_key)
+      access_token: ENV["OPENAI_API_KEY"]
     )
   end
 
