ea408dab312233481236c592e97d0e372e3b702d
pawansikarwar0207 <pawansikarwar96@gmail.com>
2025-11-27 16:42:20 +0530

    fixes for sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
+require "sidekiq/web"
 Rails.application.routes.draw do
   get 'orders/index'
   resources :hvhs_data
+  mount Sidekiq::Web => "/sidekiq"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
