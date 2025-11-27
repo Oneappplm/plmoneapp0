b4968f6310d33633698723617635f3cc292bc6aa
pawansikarwar0207 <pawansikarwar96@gmail.com>
2025-11-27 16:42:20 +0530

    fixes for sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,21 @@
-redis_url = ENV.fetch("REDIS_URL") { "redis://127.0.0.1:6379/0" }
+# config/initializers/sidekiq.rb
+
+require "sidekiq"
+
+redis_url =
+  ENV["REDIS_URL"] ||
+  "redis://default:#{ENV['REDIS_PASSWORD']}@10.0.1.213:6379/0"
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: redis_url }
+  config.redis = {
+    url: redis_url,
+    namespace: "docgo-demo"
+  }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: redis_url }
+  config.redis = {
+    url: redis_url,
+    namespace: "docgo-demo"
+  }
 end
