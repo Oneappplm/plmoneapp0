a15e0be43f10efaeffbf369672a8681698c53906
pawansikarwar0207 <pawansikarwar96@gmail.com>
2025-12-24 23:13:39 +0530

    State Licensure link feature for admin

--- app/services/webscraper/states/arkansas_scraper.rb
+++ app/services/webscraper/states/arkansas_scraper.rb
@@ -4,12 +4,12 @@ require "fileutils"
 module Webscraper
   module States
     class ArkansasScraper
-      SEARCH_URL = "https://www.armedicalboard.org/public/verify/default.aspx".freeze
+      # SEARCH_URL = "https://www.armedicalboard.org/public/verify/default.aspx".freeze
 
-      def initialize(license_number, state, url = nil)
+      def initialize(license_number, state)
         @license_number = license_number
         @state = state
-        @url = url || SEARCH_URL
+        @url = state.license_search_url
       end
 
       def call
@@ -107,7 +107,7 @@ module Webscraper
         crawler.save_screenshot(path)
 
         # 2️⃣ Add timestamp (e.g. "2025-12-18")
-        human_date = Time.current.strftime("%Y-%m-%d")
+        human_date = Time.current.strftime("%Y-%m-%d, %I:%M %p")
 
         image = MiniMagick::Image.open(path)
         image.combine_options do |c|
