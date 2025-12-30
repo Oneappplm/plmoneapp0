89b139a8f83d66900fd4210d0e1be86e325bf387
pawansikarwar0207 <pawansikarwar96@gmail.com>
2025-12-31 00:12:14 +0530

    Fixed New Changes on DocGo

--- app/controllers/mhc/provider_personal_informations_controller.rb
+++ app/controllers/mhc/provider_personal_informations_controller.rb
@@ -1,4 +1,6 @@
 class Mhc::ProviderPersonalInformationsController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:update]
+
   before_action :set_provider_personal_information, only: [:update, :update_audit_date, :submit_application]
 
   def update
