2a1b32c82812b062bec44fd0ff618e1d127cdb19
pawansikarwar0207 <pawansikarwar96@gmail.com>
2026-01-14 14:24:47 +0530

    Fixed work-tickler section on verification platform

--- app/controllers/work_ticklers_controller.rb
+++ app/controllers/work_ticklers_controller.rb
@@ -1,8 +1,11 @@
 class WorkTicklersController < ApplicationController
 
   def index
-    if params[:page]
-      render params[:page]
-    end
+    # if params[:page]
+    #   render params[:page]
+    # end
+    @q = ProviderPersonalInformation.where.not(cred_status: 'no-application').or(
+       ProviderPersonalInformation.where(cred_status: nil)
+     ).ransack(params[:q])
   end
 end
