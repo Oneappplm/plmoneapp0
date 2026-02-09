72229f8cefeed19fb145f291dfeab6ba16fa20aa
pawansikarwar0207 <pawansikarwar96@gmail.com>
2026-02-09 14:12:21 +0530

    Added Other Name for practitioner info tab

--- app/models/provider_personal_information.rb
+++ app/models/provider_personal_information.rb
@@ -70,6 +70,9 @@ class ProviderPersonalInformation < ApplicationRecord
   accepts_nested_attributes_for :provider_personal_information_credentialing_contact, allow_destroy: false, update_only: true
   accepts_nested_attributes_for :provider_personal_information_confidential_contact, allow_destroy: false, update_only: true
 
+  accepts_nested_attributes_for :provider_other_names,
+    allow_destroy: true,
+    update_only: true
 
   def fullname = "#{last_name} #{first_name} #{middle_name}"
   def correspondence_address_type_correspondence_address_type_descripion = correspondence_address_type_correspondence_address_type_descrip
