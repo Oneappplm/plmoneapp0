5e73fec3a5d44281977a69b8980b3710d00ef13f
pawansikarwar0207 <pawansikarwar96@gmail.com>
2026-04-23 14:48:15 +0530

    Fixing bulk file uploading cloudfare issue phase4

--- app/jobs/weekly_dea_import_job.rb
+++ app/jobs/weekly_dea_import_job.rb
@@ -1,39 +1,47 @@
 class WeeklyDeaImportJob < ApplicationJob
   queue_as :dea_import
 
-  def perform(upload_id)
+  def perform(upload_or_path)
     progress_key = "dea_import:#{job_id}"
 
     $redis.hset(progress_key, "status", "running")
     $redis.hset(progress_key, "last_update", Time.current.to_i)
 
-    upload = DeaImportUpload.find_by(id: upload_id)
-    raise "Upload not found" unless upload
-
     temp_file = nil
 
     file_path =
-      if upload.s3_key.present?
-        Rails.logger.info("[DEA JOB] Downloading from S3...")
-        temp_file = download_from_s3!(upload.s3_key)
+      if upload_or_path.is_a?(String)
+        # ✅ direct server path import
+        upload_or_path
       else
-        Rails.logger.info("[DEA JOB] Using local file...")
+        upload = DeaImportUpload.find_by(id: upload_or_path)
+        raise "Upload not found" unless upload
 
-        local_path =
-          if upload.file.respond_to?(:current_path) && upload.file.current_path.present?
-            upload.file.current_path
-          else
-            upload.file.path
+        if upload.s3_key.present?
+          Rails.logger.info("[DEA JOB] Downloading from S3...")
+          temp_file = download_from_s3!(upload.s3_key)
+        else
+          Rails.logger.info("[DEA JOB] Using local file...")
+
+          local_path =
+            if upload.file.respond_to?(:current_path) && upload.file.current_path.present?
+              upload.file.current_path
+            else
+              upload.file.path
+            end
+
+          if local_path.present? && !Pathname.new(local_path).absolute?
+            local_path = Rails.root.join("public", local_path).to_s
           end
 
-        if local_path.present? && !Pathname.new(local_path).absolute?
-          local_path = Rails.root.join("public", local_path).to_s
+          local_path
         end
-
-        local_path
       end
 
     raise "File missing: #{file_path}" unless file_path.present? && File.exist?(file_path)
+
+    total_bytes = File.size(file_path).to_i
+    $redis.hset(progress_key, "total_bytes", total_bytes)
 
     DeaMasterImporter.new(file_path, job_id).import!
 
