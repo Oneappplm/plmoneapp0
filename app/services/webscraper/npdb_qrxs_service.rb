c1d52fad4a864a8a6175cb79b3b159fc4fcf1917
pawansikarwar0207 <pawansikarwar96@gmail.com>
2026-06-03 13:28:29 +0530

    quick changes added for debug

--- app/services/webscraper/npdb_qrxs_service.rb
+++ app/services/webscraper/npdb_qrxs_service.rb
@@ -2,7 +2,6 @@
 
 require "nokogiri"
 require "base64"
-require "securerandom"
 require "net/http"
 require "uri"
 require "cgi"
@@ -37,6 +36,9 @@ class Webscraper::NpdbQrxsService
     send_code, send_message =
       send_submission!(creds, creds[:password], filename, submission_xml)
 
+    send_code = "NO_RESPONSE" if send_code.blank?
+    send_message = "No response/status returned from NPDB send_submission!. Check SOAP request/response parsing." if send_message.blank?
+
     debug_log("NPDB SEND CODE: #{send_code.inspect}")
     debug_log("NPDB SEND MESSAGE: #{send_message.inspect}")
 
@@ -55,7 +57,8 @@ class Webscraper::NpdbQrxsService
       rescue => e
         debug_error(e)
         failed = true
-        send_message = e.message
+        send_code = "RECEIVE_EXCEPTION"
+        send_message = "#{e.class}: #{e.message}"
       end
     end
 
@@ -102,17 +105,19 @@ class Webscraper::NpdbQrxsService
     raise "Missing last name" if @ppi.last_name.blank?
     raise "Missing SSN" if ssn_digits.blank?
     raise "Missing birth date" if birth_date.blank?
-
     raise "Missing address" if @ppi.address_line1.blank?
     raise "Missing city" if @ppi.city.blank?
     raise "Missing state" if @ppi.state.blank?
-    raise "Missing ZIP+4" unless zip_digits.length == 9
+
+    unless zip_digits.length == 9
+      raise "ERROR 81: Missing ZIP+4. Current ZIP is #{@ppi.zipcode.inspect}. Production NPDB query requires 9-digit ZIP+4."
+    end
 
     raise "Missing sex" if sex_value.blank?
     raise "Missing license" unless selected_license.present?
     raise "Missing license number" if selected_license.license_number.blank?
     raise "Missing license state" if selected_license_state.blank?
-    raise "Missing NPDB occupation/field code" if occupation_field_code.blank?
+    raise "ERROR B1: Missing NPDB occupation/field code" if occupation_field_code.blank?
   end
 
   def validate_npdb_payload!
@@ -123,10 +128,6 @@ class Webscraper::NpdbQrxsService
   def build_submission_xml
     zip5, zip4 = normalized_zip_parts(@ppi.zipcode)
 
-    street = normalize_address(@ppi.address_line1)
-    city   = normalize_city(@ppi.city)
-    state  = normalize_state(@ppi.state)
-
     cert_name = [
       ENV["NPDB_CERTIFIER_NAME"].presence || @ppi.first_name,
       ENV["NPDB_CERTIFIER_MIDDLE"].presence,
@@ -134,9 +135,6 @@ class Webscraper::NpdbQrxsService
     ].compact.join(" ").upcase
 
     license = selected_license
-    license_number = license.license_number.to_s.upcase.gsub(/[^A-Z0-9]/, "")
-    license_state  = selected_license_state
-    occupation     = occupation_field_code
 
     <<~XML
       <?xml version="1.0" encoding="UTF-8"?>
@@ -178,18 +176,18 @@ class Webscraper::NpdbQrxsService
           <ssn>#{ssn_digits}</ssn>
 
           <workAddress>
-            <address>#{xml_escape(street)}</address>
+            <address>#{xml_escape(normalize_address(@ppi.address_line1))}</address>
             #{address2_xml}
-            <city>#{xml_escape(city)}</city>
-            <state>#{xml_escape(state)}</state>
+            <city>#{xml_escape(normalize_city(@ppi.city))}</city>
+            <state>#{xml_escape(normalize_state(@ppi.state))}</state>
             <zip>#{zip5}</zip>
             <zip4>#{zip4}</zip4>
           </workAddress>
 
           <occupationAndLicensure>
-            <number>#{xml_escape(license_number)}</number>
-            <state>#{xml_escape(license_state)}</state>
-            <field>#{xml_escape(occupation)}</field>
+            <number>#{xml_escape(license.license_number.to_s.upcase.gsub(/[^A-Z0-9]/, ""))}</number>
+            <state>#{xml_escape(selected_license_state)}</state>
+            <field>#{xml_escape(occupation_field_code)}</field>
           </occupationAndLicensure>
         </individual>
 
@@ -197,15 +195,95 @@ class Webscraper::NpdbQrxsService
     XML
   end
 
+  def send_submission!(creds, password, filename, xml)
+    uri = URI(endpoint)
+
+    soap_body = <<~XML
+      <?xml version="1.0" encoding="UTF-8"?>
+      <soap:Envelope
+        xmlns:soap="http://www.w3.org/2003/05/soap-envelope"
+        xmlns:qrx="#{NAMESPACE}">
+        <soap:Header/>
+        <soap:Body>
+          <qrx:Send>
+            <qrx:DataBankID>#{xml_escape(creds[:dbid])}</qrx:DataBankID>
+            <qrx:Password>#{xml_escape(password)}</qrx:Password>
+            <qrx:UserID>#{xml_escape(creds[:user_id])}</qrx:UserID>
+            <qrx:SubmissionFiles>
+              <FileName>#{xml_escape(filename)}</FileName>
+              <XmlFileData>#{Base64.strict_encode64(xml)}</XmlFileData>
+            </qrx:SubmissionFiles>
+          </qrx:Send>
+        </soap:Body>
+      </soap:Envelope>
+    XML
+
+    debug_xml("NPDB SOAP SEND REQUEST", soap_body)
+
+    request = Net::HTTP::Post.new(uri)
+    request["Content-Type"] = 'application/soap+xml; charset=UTF-8; action="Send"'
+    request.body = soap_body
+
+    response =
+      Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == "https") do |http|
+        http.open_timeout = 30
+        http.read_timeout = 120
+        http.request(request)
+      end
+
+    debug_log("NPDB HTTP STATUS: #{response.code} #{response.message}")
+    debug_xml("NPDB SOAP SEND RAW RESPONSE", response.body)
+
+    unless response.is_a?(Net::HTTPSuccess)
+      return ["HTTP_#{response.code}", "HTTP error from NPDB: #{response.code} #{response.message}"]
+    end
+
+    parse_qrxs_status(response.body)
+  rescue => e
+    debug_error(e)
+    ["EXCEPTION", "#{e.class}: #{e.message}"]
+  end
+
+  def parse_qrxs_status(xml)
+    doc = Nokogiri::XML(xml.to_s)
+    doc.remove_namespaces!
+
+    fault = doc.at_xpath("//Fault")
+    if fault
+      fault_message =
+        fault.at_xpath(".//Reason//Text")&.text.presence ||
+        fault.at_xpath(".//faultstring")&.text.presence ||
+        fault.text.squish
+
+      return ["SOAP_FAULT", fault_message]
+    end
+
+    code =
+      doc.at_xpath("//StatusCode")&.text.presence ||
+      doc.at_xpath("//statusCode")&.text.presence ||
+      doc.at_xpath("//Code")&.text.presence ||
+      doc.at_xpath("//code")&.text.presence
+
+    message =
+      doc.at_xpath("//StatusMessage")&.text.presence ||
+      doc.at_xpath("//statusMessage")&.text.presence ||
+      doc.at_xpath("//Message")&.text.presence ||
+      doc.at_xpath("//message")&.text.presence ||
+      doc.text.squish
+
+    [code, message]
+  end
+
+  def receive_poll!(_creds, _password)
+    []
+  end
+
   def birthdate_xml(value)
     date =
       case value
-      when Date
-        value
-      when Time, ActiveSupport::TimeWithZone
-        value.to_date
-      else
-        Date.parse(value.to_s)
+      when Date then value
+      when Time, ActiveSupport::TimeWithZone then value.to_date
+      else Date.parse(value.to_s)
       end
 
     "<birthdate>#{date.strftime('%Y-%m-%d')}</birthdate>"
@@ -213,95 +291,63 @@ class Webscraper::NpdbQrxsService
     raise "Invalid birth date format"
   end
 
-  def ssn_digits
-    @ppi.ssn.to_s.gsub(/\D/, "")
-  end
-
-  def zip_digits
-    @ppi.zipcode.to_s.gsub(/\D/, "")
-  end
+  def ssn_digits = @ppi.ssn.to_s.gsub(/\D/, "")
+  def zip_digits = @ppi.zipcode.to_s.gsub(/\D/, "")
+  def birth_date = @ppi.birth_date || @ppi.date_of_birth
 
   def normalized_zip_parts(value)
     digits = value.to_s.gsub(/\D/, "")
     raise "ERROR 81: ZIP must be 9 digits ZIP+4" unless digits.length == 9
-
     [digits[0, 5], digits[5, 4]]
-  end
-
-  def birth_date
-    @ppi.birth_date || @ppi.date_of_birth
   end
 
   def sex_value
     value = @ppi.gender.to_s.strip.downcase
     return "M" if value.start_with?("m")
     return "F" if value.start_with?("f")
-
     nil
   end
 
   def occupation_field_code
-    map_field_code(
-      @ppi.provider_type_provider_type_abbreviation.presence ||
-      selected_license&.license_type
-    )
+    map_field_code(@ppi.provider_type_provider_type_abbreviation.presence || selected_license&.license_type)
   end
 
   def map_field_code(value)
     case value.to_s.downcase.strip
-    when /\bmd\b|medical doctor|physician/
-      "114"
-    when /\bdo\b|osteopathic/
-      "115"
-    when /\bdds\b|dentist/
-      "122"
-    when /\bnp\b|nurse practitioner/
-      "117"
-    when /\bpa\b|physician assistant/
-      "118"
-    when /\brn\b|registered nurse/
-      "119"
-    else
-      nil
+    when /\bmd\b|medical doctor|physician/ then "114"
+    when /\bdo\b|osteopathic/ then "115"
+    when /\bdds\b|dentist/ then "122"
+    when /\bnp\b|nurse practitioner/ then "117"
+    when /\bpa\b|physician assistant/ then "118"
+    when /\brn\b|registered nurse/ then "119"
+    else nil
     end
   end
 
   def normalize_address(value)
-    value.to_s.upcase.strip
-      .gsub(/\bST\b/, "STREET")
-      .gsub(/\bRD\b/, "ROAD")
-      .gsub(/\bLN\b/, "LANE")
+    value.to_s.upcase.strip.gsub(/\bST\b/, "STREET").gsub(/\bRD\b/, "ROAD").gsub(/\bLN\b/, "LANE")
   end
 
-  def normalize_city(value)
-    value.to_s.upcase.strip
-  end
-
-  def normalize_state(value)
-    value.to_s.upcase.strip
-  end
+  def normalize_city(value) = value.to_s.upcase.strip
+  def normalize_state(value) = value.to_s.upcase.strip
 
   def middle_name_xml
     return "" if @ppi.middle_name.blank?
-
     "<middle>#{xml_escape(@ppi.middle_name.to_s.upcase)}</middle>"
   end
 
   def suffix_xml
     return "" if @ppi.suffix.blank?
-
     "<suffix>#{xml_escape(@ppi.suffix.to_s.upcase)}</suffix>"
   end
 
   def address2_xml
     return "" if @ppi.address_line2.blank?
-
     "<address2>#{xml_escape(normalize_address(@ppi.address_line2))}</address2>"
   end
 
   def agent_dbid_xml
     return "" if ENV["NPDB_AGENT_DBID"].blank?
-
     "<agentDBID>#{xml_escape(ENV["NPDB_AGENT_DBID"])}</agentDBID>"
   end
 
@@ -312,9 +358,7 @@ class Webscraper::NpdbQrxsService
   end
 
   def selected_license
-    @selected_license ||=
-      @ppi.provider_licensures.find(&:is_primary_license) ||
-      @ppi.provider_licensures.first
+    @selected_license ||= @ppi.provider_licensures.find(&:is_primary_license) || @ppi.provider_licensures.first
   end
 
   def selected_license_state
@@ -331,14 +375,6 @@ class Webscraper::NpdbQrxsService
     }
   end
 
-  def send_submission!(creds, password, filename, xml)
-    # keep your existing implementation
-  end
-
-  def receive_poll!(creds, password)
-    # keep your existing implementation
-  end
-
   def build_error_xml(code, message)
     <<~XML
       <npdbError>
@@ -353,9 +389,7 @@ class Webscraper::NpdbQrxsService
     ENV["NPDB_ENV"].to_s.downcase == "production" ? PROD_ENDPOINT : QA_ENDPOINT
   end
 
-  def xml_escape(value)
-    CGI.escapeHTML(value.to_s)
-  end
+  def xml_escape(value) = CGI.escapeHTML(value.to_s)
 
   def debug_log(message)
     Rails.logger.info(message)
