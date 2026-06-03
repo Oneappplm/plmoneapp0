94e62978ce7bd0021c0be52f76d6268cfdd6112d
pawansikarwar0207 <pawansikarwar96@gmail.com>
2026-06-03 10:15:22 +0000

    Remove duplicate migration classes

--- db/migrate/20260522125043_add_legacy_client_name_to_provider_personal_informations.rb
+++
@@ -1,5 +0,0 @@
-class AddLegacyClientNameToProviderPersonalInformations < ActiveRecord::Migration[7.0]
-  def change
-    add_column :provider_personal_informations, :legacy_client_name, :string
-  end
-end
