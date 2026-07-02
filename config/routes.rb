9ddeae395a6bd0f18e7b949ccbfeb44983eb0756
pawansikarwar0207 <pawansikarwar96@gmail.com>
2026-07-02 15:39:17 +0530

    OPMC tab phase1

--- config/routes.rb
+++ config/routes.rb
@@ -147,6 +147,7 @@ Rails.application.routes.draw do
 
   namespace :mhc do
     resources :provider_personal_information_omigs, only: [:create, :update]
+    resources :provider_personal_information_opmcs, only: [:create, :update]
     
    get  :reports, to: "reports#index"
 
